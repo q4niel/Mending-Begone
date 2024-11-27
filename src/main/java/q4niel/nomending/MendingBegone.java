bd28b385f27e949a292b560847a761bb31e5d0ed
q4niel <github.obscure938@passmail.net>
2024-11-27 16:21:49 +0100

    Fabric Mod Template

---
+++ src/main/java/q4niel/nomending/MendingBegone.java
@@ -0,0 +1,14 @@
+package q4niel.nomending;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MendingBegone implements ModInitializer {
+	public static final String MOD_ID = "nomending";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {}
+}
---
+++ src/main/java/q4niel/nomending/MendingBegoneDataGenerator.java
@@ -0,0 +1,9 @@
+package q4niel.nomending;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MendingBegoneDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {}
+}
